78f676ce8a0900e0f6783fc9e32bacdf297af0e2
shamimhcp1 <shamimhcp1@gmail.com>
2023-11-12 03:58:48 +0600

    fix valid flight check

--- flights/models.py
+++ flights/models.py
@@ -17,7 +17,7 @@ class Flight(models.Model):
         return f"{self.id} : {self.origin} to {self.destination}"
 
     def is_valid_flight(self):
-        return self.origin != self.destination or self.duration > 0
+        return self.origin != self.destination and self.duration > 0
     
 class Passengers(models.Model):
     first = models.CharField(max_length=64)
